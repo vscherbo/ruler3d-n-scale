c561f270372b0ea31110580e0403024c27c06e7a
Vladimir Scherbo <vscherbo@gmail.com>
2025-01-17 15:22:55 +0000

    PK: shp_id, box like in shp.wlwh

--- cr-ruler3d.sql
+++ cr-ruler3d.sql
@@ -1,10 +1,15 @@
+-- shp.ruler3d определение
+
+-- Drop table
+
+-- DROP TABLE shp.ruler3d;
+
 CREATE TABLE shp.ruler3d (
-	id serial NOT NULL,
-	box_id varchar NOT NULL,
+	shp_id int4 NOT NULL,
+	box int4 NOT NULL,
 	length numeric NULL,
 	width numeric NULL,
 	height numeric NULL,
-	ins_ts timestamp without time zone DEFAULT now () NOT NULL,
-	CONSTRAINT ruler3d_pk PRIMARY KEY (id)
+	ins_ts timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT ruler3d_pk PRIMARY KEY (shp_id, box)
 );
-
